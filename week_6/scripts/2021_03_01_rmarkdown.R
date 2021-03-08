6089cd3171bf17b5760fe0345a215feb0fb3b206
aag19469 <anthony.gutierrez.913@my.csun.edu>
2021-03-07 22:10:38 -0800

    committing week 6 files

---
+++ week_6/scripts/2021_03_01_rmarkdown.R
@@ -0,0 +1,32 @@
+### I will be using R markdown for the first time 
+### Using rmarkdown and tinytex packages
+### Created by: Anthony Gutierrez 
+### Created on: 2021_03_01
+### Updated on: 2021_03_
+
+### Notes #####
+#only need two pieces of work: Title and output
+
+#if you want to create the html file, it is calling knitting it
+
+#Header decides 
+
+# if you want a new line, need two spaces between sentences
+
+#For images: ![](link or directory)
+
+#Code chunks: keyboard shortcut is Ctl alt I
+
+### Totally Awesome R package #####
+library(beepr)
+beep(1)
+beep(2)
+beep(3)
+beep(4)
+beep(5)
+beep(6)
+beep(7)
+beep(8)
+beep(9)
+beep(10)
+
